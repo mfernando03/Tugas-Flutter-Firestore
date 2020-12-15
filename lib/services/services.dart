04858e18eabd68c5342be3e5913c0a6b0e47ec17
mfernando03 <mfernando03@student.ciputra.ac.id>
2020-12-15 21:31:31 +0700

    Flutter Firestore

---
+++ lib/services/services.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:new_bloc/models/models.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:new_bloc/extensions/extensions.dart';
+import 'dart:core';
+
+part 'auth_services.dart';
+part 'user_services.dart';
+part 'product_services.dart';
---
+++ lib/ui/widget/widget.dart
@@ -0,0 +1,7 @@
+import 'package:new_bloc/models/models.dart';
+import 'package:new_bloc/ui/pages/pages.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+part 'productcard.dart';
